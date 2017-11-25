e3d22a3aac4c276bb564e9b51f8b4717d9831a94
siliconrhode <siliconrhode@gmail.com>
2017-11-25 13:11:31 -0500

    variables

---
+++ variables/variables/variables/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System;
+
+namespace variables
+{
+    class Variables
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            Console.WriteLine(number);
+        }
+    }
+}
