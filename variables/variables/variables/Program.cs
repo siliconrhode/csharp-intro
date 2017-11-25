a7f803a4f4c645fb854b929d828fbb39be2c63bf
siliconrhode <siliconrhode@gmail.com>
2017-11-25 17:31:57 -0500

    print mult variables

--- variables/variables/variables/Program.cs
+++ variables/variables/variables/Program.cs
@@ -9,7 +9,17 @@ namespace variables
         static void Main(string[] args)
         {
             byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Mosh";
+            bool isWorking = false;
             Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
         }
     }
 }
