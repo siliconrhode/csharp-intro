786ef23858e3806c522b3247eed4e5d4a819fe4a
siliconrhode <siliconrhode@gmail.com>
2017-11-25 17:54:03 -0500

    byte, float MinValue-MaxValue

--- variables/variables/variables/Program.cs
+++ variables/variables/variables/Program.cs
@@ -8,18 +8,9 @@ namespace variables
     {
         static void Main(string[] args)
         {
-            var number = 2;
-            var count = 10;
-            var totalPrice = 20.95f;
-            var character = 'A';
-            var firstName = "Mosh";
-            var isWorking = false;
-            Console.WriteLine(number);
-            Console.WriteLine(count);
-            Console.WriteLine(totalPrice);
-            Console.WriteLine(character);
-            Console.WriteLine(firstName);
-            Console.WriteLine(isWorking);
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
         }
     }
 }
