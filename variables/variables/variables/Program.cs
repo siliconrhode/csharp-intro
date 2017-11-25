c8f9c04968b100c22779bab9cec76d2cecebf17d
siliconrhode <siliconrhode@gmail.com>
2017-11-25 17:34:36 -0500

    use var to declate different data types

--- variables/variables/variables/Program.cs
+++ variables/variables/variables/Program.cs
@@ -8,12 +8,12 @@ namespace variables
     {
         static void Main(string[] args)
         {
-            byte number = 2;
-            int count = 10;
-            float totalPrice = 20.95f;
-            char character = 'A';
-            string firstName = "Mosh";
-            bool isWorking = false;
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Mosh";
+            var isWorking = false;
             Console.WriteLine(number);
             Console.WriteLine(count);
             Console.WriteLine(totalPrice);
