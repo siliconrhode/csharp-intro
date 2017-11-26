3b23bc17f8a16365067ab85db8c7fccb361bc857
siliconrhode <siliconrhode@gmail.com>
2017-11-26 16:05:39 -0500

    System.OverflowException

---
+++ typeConversion/typeConversion/typeConversion/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace typeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = "1234";
+            // this will cause System.OverflowException
+            byte b = Convert.ToByte(number);
+            Console.WriteLine(b);
+
+        }
+    }
+}
--- variables/variables/variables/Program.cs
+++ variables/variables/variables/Program.cs
@@ -8,9 +8,12 @@ namespace variables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
-            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
-
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Mosh";
+            Console.WriteLine(number);
         }
     }
 }
