d993037976800bbc1fe8a23f2c8a93ec88f5b5b7
siliconrhode <siliconrhode@gmail.com>
2017-11-28 11:59:22 -0500

    array of type bool, array of type string

--- Arrays/Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Arrays/Program.cs
@@ -18,7 +18,15 @@ namespace Arrays
             Console.WriteLine(numbers[1]);
             Console.WriteLine(numbers[2]);
 
-            //var flags = new[3]
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[3] { "Sam", "Ham","Dam"};
+
 
 
         }
