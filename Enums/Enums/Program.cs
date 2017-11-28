2959232ebc580cb7b2ba299410c48589bc7c1a73
siliconrhode <siliconrhode@gmail.com>
2017-11-28 14:38:06 -0500

    create and print enums

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Enums
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail =1,
+        RegisteredAirMail = 2 ,
+        Express = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+        }
+    }
+}
