5fbbc18a89ec33384f477c7e34dd8c6b76b098cd
siliconrhode <siliconrhode@gmail.com>
2017-11-28 13:43:34 -0500

    create an array and join its elements using string.Join

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create an array 
+            var numbers = new int[3] {1, 2.3};
+            // join the aray elements 
+            string list = string.Join(",", numbers);
+
+        }
+    }
+}
