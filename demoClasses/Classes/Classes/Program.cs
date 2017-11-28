59647676c2a51009f310199751d8a75976a9fd47
siliconrhode <siliconrhode@gmail.com>
2017-11-28 10:12:30 -0500

    Alt Enter to move a class to .cs file

--- demoClasses/Classes/Classes/Program.cs
+++ demoClasses/Classes/Classes/Program.cs
@@ -1,18 +1,6 @@
 ﻿
-using System;
-
 namespace Classes
 {
-    public class Person
-    {
-        public string FirstName;
-        public string LastName;
-
-        public void Introduce()
-        {
-            Console.WriteLine("Hi, my first name is:" + FirstName + " last name is:"+LastName);
-        }
-    }
     class Program
     {
         static void Main(string[] args)
