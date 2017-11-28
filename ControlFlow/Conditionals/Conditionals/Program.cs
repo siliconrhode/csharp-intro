e1929a85e51bc94290981a7ad6a8e30e29ad3470
siliconrhode <siliconrhode@gmail.com>
2017-11-28 16:44:52 -0500

    Switch/Case

--- ControlFlow/Conditionals/Conditionals/Program.cs
+++ ControlFlow/Conditionals/Conditionals/Program.cs
@@ -8,14 +8,21 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            int hour = 10;
+            var season = Season.Autumn;
 
-            if (hour > 0 && hour < 12)
-                Console.WriteLine("It's morning.");
-            else if ( hour > 12 && hour <18)
-                Console.WriteLine("It's  afternoon");
-            else
-                Console.WriteLine("It's evening");
+            switch (season)
+            {
+                case Season.Autumn:
+                    Console.WriteLine("It's Autumn");
+                    break;
+                case Season.Summer:
+                    Console.WriteLine("It's Summer");
+                    break;
+
+                default:
+                    Console.WriteLine("Everything else sucks");
+                    break;
+            }
         }
     }
 }
