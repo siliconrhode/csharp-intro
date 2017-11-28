75b63c8e32a6823564bf6c18554d1f6803bbf160
siliconrhode <siliconrhode@gmail.com>
2017-11-28 16:23:00 -0500

    If/Else

---
+++ ControlFlow/Conditionals/Conditionals/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 10;
+
+            if (hour > 0 && hour < 12)
+                Console.WriteLine("It's morning.");
+            else if ( hour > 12 && hour <18)
+                Console.WriteLine("It's  afternoon");
+            else
+                Console.WriteLine("It's evening");
+        }
+    }
+}
--- referenceTypesValueTypes/referenceTypes/referenceTypes/Program.cs
+++ referenceTypesValueTypes/referenceTypes/referenceTypes/Program.cs
@@ -3,20 +3,30 @@ using System;
 
 namespace referenceTypes
 {
+    public class Person
+    {
+        public int Age;
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            var a = 10;
-            var b = a;
-            b++;
-            Console.WriteLine(string.Format("a:{0} b:{1}",a,b));
+            var number = 10;
+            Increment(number);
+            Console.WriteLine(number);
+            var person = new Person() {Age = 20};
+            MakeOld(person);
+            Console.WriteLine(person.Age);
+        }
 
-            var array1 = new int[3] {1, 2, 3};
-            var array2 = array1;
-            array2[0] = 0;
-            Console.WriteLine(string.Format("array1[0]: {0} array2[0]: {1}",array1[0],array2[0]));
+        public static void Increment(int number)
+        {
+            number += 10;
+        }
 
+        public static void MakeOld(Person person)
+        {
+            person.Age += 10;
         }
     }
 }
