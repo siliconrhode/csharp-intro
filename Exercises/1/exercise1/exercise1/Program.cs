e308b911e5d67d817f0c1e0d1b87b31937212c27
siliconrhode <siliconrhode@gmail.com>
2017-11-29 17:54:13 -0500

    The maximum of two numbers

--- Exercises/1/exercise1/exercise1/Program.cs
+++ Exercises/1/exercise1/exercise1/Program.cs
@@ -11,7 +11,9 @@ namespace exercise1
     {
         static void Main(string[] args)
         {
-            OneToTen();
+            //OneToTen();
+            MaxOfTwo();
+
         }
 
         public static void OneToTen()
@@ -28,5 +30,24 @@ namespace exercise1
                 Console.WriteLine("Invalid");
             }
         }
+
+        public static void MaxOfTwo()
+        {
+            Console.WriteLine("Please enter the first number");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (firstNumber > secondNumber)
+            {
+                Console.WriteLine("The biggest number is{0}",firstNumber);
+            }
+            else
+            {
+                Console.WriteLine("The maximum number is {0}",secondNumber);
+            }
+    }
+
+
     }
 }
