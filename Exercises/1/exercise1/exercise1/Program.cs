7d72d9a4d484fa225b4c3fc110b9aca5f0ebf713
siliconrhode <siliconrhode@gmail.com>
2017-11-29 11:43:03 -0500

    Enter a number  from 1 to 10

---
+++ Exercises/1/exercise1/exercise1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number >= 1 && number <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        ;
+    }
+    }
+}
