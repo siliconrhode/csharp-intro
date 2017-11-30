79aa959c58796f53dc732eba7cd1c0f3e03ba05e
siliconrhode <siliconrhode@gmail.com>
2017-11-30 00:18:54 -0500

    Print even nubers from 10 to 1, and vice versa

---
+++ Iteration Statements/iterationStatements/iterationStatements/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace iterationStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print even numbers from 1 to 10;
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            //Print even numbers from 10 to 1;
+            for (int i = 10; i >= 1; i--)
+            {   if (i%2 == 0)
+                Console.WriteLine(i);
+            }
+                
+        }
+
+    }
+
+}
