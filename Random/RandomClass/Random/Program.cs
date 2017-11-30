ea71c8bd45383e8af27799bd1b90ef39b2f96495
siliconrhode <siliconrhode@gmail.com>
2017-11-30 13:08:13 -0500

    Generate five random numbers

---
+++ Random/RandomClass/Random/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new System.Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(random.Next());
+            }
+
+
+        }
+    }
+}
