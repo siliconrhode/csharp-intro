8daf7cf2e0a845c4f21c055b0d99d68e99fd3913
siliconrhode <siliconrhode@gmail.com>
2017-12-03 14:17:08 -0500

    move summerize method into a class

--- SummarizingText/SummarizingText/SummarizingText/Program.cs
+++ SummarizingText/SummarizingText/SummarizingText/Program.cs
@@ -11,38 +11,10 @@ namespace SummarizingText
         static void Main(string[] args)
         {
             var sentence = " This is going to be a very very very really really long text.";
-            var summary = SummerizeText(sentence, 25);
+            var summary = StringUtility.SummerizeText(sentence, 25);
             Console.WriteLine(summary);
 
         }
 
-        static string SummerizeText(string text, int maxLength = 20)
-        {
-            if (text.Length < maxLength)
-            {
-                return text;
-            }
-
-            
-            
-            var words = text.Split(' ');
-            var totalLength = 0;
-            var summaryList = new List<string>();
-            foreach (var word in words)
-            {
-
-                summaryList.Add(word);
-                totalLength += word.Length;
-                if (totalLength > maxLength)
-                {
-                    break;
-                }
-            }
-            
-            return String.Join(" ", summaryList) + "...";
-
-            
-
-        }
     }
 }
---
+++ SummarizingText/SummarizingText/SummarizingText/StringUtility.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummarizingText
+{
+    public class StringUtility
+    {
+        public static string SummerizeText(string text, int maxLength = 20)
+        {
+            if (text.Length < maxLength)
+            {
+                return text;
+            }
+
+
+
+            var words = text.Split(' ');
+            var totalLength = 0;
+            var summaryList = new List<string>();
+            foreach (var word in words)
+            {
+
+                summaryList.Add(word);
+                totalLength += word.Length;
+                if (totalLength > maxLength)
+                {
+                    break;
+                }
+            }
+
+            return String.Join(" ", summaryList) + "...";
+
+
+
+        }
+
+    }
+}
