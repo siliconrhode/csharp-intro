faa357e2b2ab0b1bd0aa0347b93f37d03a47962a
siliconrhode <siliconrhode@gmail.com>
2017-12-03 14:58:01 -0500

    string manipulation

--- StringBuilder/StringBuilder/StringBuilder/Program.cs
+++ StringBuilder/StringBuilder/StringBuilder/Program.cs
@@ -13,7 +13,13 @@ namespace UsingStringBuilder
             builder.Append("Header");
             builder.AppendLine();
             builder.Append('-',10);
+
+            builder.Replace('-', '+');
+            builder.Remove(0, 10);
+            builder.Insert(0, new string('-', 10));
             Console.WriteLine(builder);
+
+            
 
         }
     }
