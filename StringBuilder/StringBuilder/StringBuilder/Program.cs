f35b5331366a3c0ac7cebd8eed52a88fc76407db
siliconrhode <siliconrhode@gmail.com>
2017-12-03 14:46:34 -0500

    StringBuilder

---
+++ StringBuilder/StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace UsingStringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder();
+            builder.Append('-',10);
+            builder.AppendLine();
+            builder.Append("Header");
+            builder.AppendLine();
+            builder.Append('-',10);
+            Console.WriteLine(builder);
+
+        }
+    }
+}
