937be39d5ebb48c9ee6494ce65bf39b46be1a2cc
siliconrhode <siliconrhode@gmail.com>
2017-12-04 12:15:21 -0500

    Duplicate or non duplicate

--- Section8-Exercises/Exercises/Exercises/Program.cs
+++ Section8-Exercises/Exercises/Exercises/Program.cs
@@ -10,6 +10,11 @@ namespace Exercises
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Duplicate());
+        }
+
+        static string AscendingOrDescending()
+        {
             Console.WriteLine("Please Enter numbers separated by '-' ");
             var userInput = Console.ReadLine();
 
@@ -27,10 +32,10 @@ namespace Exercises
 
             }
             var ascending = true;
-            for (var i = 0; i < inputArray.Length-1; i++)
+            for (var i = 0; i < inputArray.Length - 1; i++)
             {
 
-                if (!( Convert.ToInt32(inputArray[i + 1]) > Convert.ToInt32(inputArray[i])))
+                if (!(Convert.ToInt32(inputArray[i + 1]) > Convert.ToInt32(inputArray[i])))
                 {
                     ascending = false;
                     break;
@@ -40,12 +45,39 @@ namespace Exercises
 
             if (ascending || descending)
             {
-                Console.WriteLine("Consecutive");
+                return "Ascending";
             }
             else
             {
-                Console.WriteLine("Not Consecutive");
+                return "Descending";
             }
+
         }
+
+        public static string Duplicate()
+        {
+            Console.WriteLine("Please enter numbers separated by a hyphen");
+            var userInput = Console.ReadLine();
+            if (String.IsNullOrWhiteSpace(userInput))
+            {
+                Console.WriteLine("Ext");
+            }
+            var numbers = new List<int>();
+            foreach (var number in userInput.Split('-'))
+            {
+                numbers.Add(Convert.ToInt32(number));
+            }
+            numbers.Sort();
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i - 1] == numbers[i])
+                {
+                    return "Duplicate";
+                }
+            }
+            return "Non Duplicate";
+        }
+
+
     }
 }
