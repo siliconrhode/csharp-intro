c355f263e95382aebebe9ad3990ae3b604c791c5
siliconrhode <siliconrhode@gmail.com>
2017-12-04 16:44:30 -0500

    count words

--- ProcedualProgramming/ProcedualProgramming/ProcedualProgramming/Program.cs
+++ ProcedualProgramming/ProcedualProgramming/ProcedualProgramming/Program.cs
@@ -22,6 +22,7 @@ namespace ProcedualProgramming
             {
                 array[name.Length - i] = name[i - 1];
             }
+
             return new string(array);
         }
     }
---
+++ Section-9-Exercises/WorkingWithFiles-Exercises/WorkingWithFiles-Exercises/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System;
+using System.IO;
+
+namespace WorkingWithFiles_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileLocation = (@"C:\Users\Widows Pc\Documents\NoRuleModels.txt");
+            var content = File.ReadAllText(fileLocation);
+            Console.WriteLine(CountWords(content));
+        }
+
+        public static int CountWords(string text)
+        {
+            var wordCount = 0;
+            foreach (var word in text)
+            {
+                wordCount++;
+            }
+            return wordCount;
+        }
+
+    }
+}
